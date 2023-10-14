1d34ee3d6098e4724e475dbbd1aa78b14ffcc612
Pree46 <preethirajagopal464@gmail.com>
2023-10-14 22:18:26 +0530

    Functionality to display the added task created

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("")
+  const handleSubmit = e => {
+    e.preventDefault();
+    
+    addTodo(value);
+
+
+    setValue("")
+  }
 
     return (
-      <form className='ToDoForm'>
-        <input type='text' className='todo-input' placeholder='Type your Task here...'/>
+      <form className='ToDoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='Type your Task here...'
+        onChange={(e)=> setValue(e.target.value)}/>
         <button type='submit' className='todo-btn'>+</button>
       </form>
     );
